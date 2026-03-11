a548b2cc523a5cfb2f2996ba82638c76a9004307
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2026-03-11 21:53:45 +0100

    fix: restore calculateEventPrice as local const (was broken by export)

--- api/controllers/events.controllers.js
+++ api/controllers/events.controllers.js
@@ -46,7 +46,7 @@ const validateEventData = (data) => {
 
 // Helper to calculate price based on config and event data
 // NOW MUTATES eventData to add snapshots if they are missing!
-module.exports.calculateEventPrice = async (eventData, config) => {
+const calculateEventPrice = async (eventData, config) => {
   const { tipo, fecha, turno, detalles, horario } = eventData;
   if (tipo === 'bloqueo') return 0;
 
